c2fb803957d379f44ba42319f5fdc219d9113b5c
stepchig <stepchig@mail.ru>
2018-01-01 14:56:54 +0200

    start

---
+++ TestProject/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string str;
+
+	cin >> str;
+	
+	cout << "You'r string: " << str << endl;
+
+	system("pause");
+}
